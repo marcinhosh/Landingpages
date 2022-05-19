e7eaa1a89624215d215326f74c3b4cc2c091c165
Marcio <marcinhostudiopro@gmail.com>
2022-05-19 09:37:08 -0300

    configuracao google fonts

--- frontend/src/styles/global-style.js
+++ frontend/src/styles/global-style.js
@@ -1,13 +1,24 @@
 import { createGlobalStyle, css } from "styled-components";
 
+
+
 export const GlobalStyle = createGlobalStyle`
-    * {
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
+* {
         margin: 12px;
         padding: 0;
     }    
+    html {
+     font-size: 62.5%;
+   }
     body {
-        ${({ theme }) => css`
-        background: ${theme.colors.mainBg} 
-    `}
-    }
+     font-size: 1.6rem;
+     font-family: ${({ theme }) => theme.font.family.default};
+   }
+
+    h1 {
+     font-family: ${({ theme }) => theme.font.family.secondary};
+   }
 `
--- frontend/src/styles/theme.js
+++ frontend/src/styles/theme.js
@@ -3,5 +3,11 @@ export const theme = {
         primaryColor: '#0A1128',
         secondaryColor: '#dc143c',
         white: '#FFFFFF',
-    }
+    },
+    font: {
+        family: {
+            default: "'Open Sans', sans-serif",
+            secondary: "'Montserrat', sans-serif",
+        },
+    },
 };
