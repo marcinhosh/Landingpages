1f5fc3cf90034e7f47794df2ba11f39cd5d32255
Marcio <marcinhostudiopro@gmail.com>
2022-05-23 08:51:30 -0300

    heading component 1 e 2

--- frontend/src/Components/Heading/styles.jsx
+++ frontend/src/Components/Heading/styles.jsx
@@ -1,7 +1,34 @@
 import styled, { css } from 'styled-components';
 
-export const Title = styled.h1`
-${({ theme, light }) => css`
-     color: ${light ? theme.colors.white : theme.colors.primaryColor};
-        `}
+const titleSize = {
+  small: (theme) => css`
+     font-size: ${theme.font.sizes.medium};
+   `,
+  medium: (theme) => css`
+     font-size: ${theme.font.sizes.large};
+   `,
+  big: (theme) => css`
+     font-size: ${theme.font.sizes.xlarge};
+   `,
+  huge: (theme) => css`
+     font-size: ${theme.font.sizes.xhuge};
+     ${mediaFont(theme)};
+   `,
+};
+
+const mediaFont = (theme) => css`
+@media ${theme.media.lteMedium} {
+  font-size: ${theme.font.sizes.xlarge};
+}`;
+
+const titleCase = (uppercase) => css`
+   text-transform: ${uppercase ? 'uppercase' : 'none'};
  `;
+
+export const Title = styled.h1`
+${({ theme, colorDark, size, uppercase }) => css`
+   color: ${colorDark ? theme.colors.primaryColor : theme.colors.white};
+   ${titleSize[size](theme)};
+   ${titleCase(uppercase)};
+`}
+`;
--- frontend/src/styles/global-style.js
+++ frontend/src/styles/global-style.js
@@ -4,8 +4,6 @@ import { createGlobalStyle, css } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
 
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
-
 * {
         margin: 12px;
         padding: 0;
@@ -18,7 +16,7 @@ export const GlobalStyle = createGlobalStyle`
      font-family: ${({ theme }) => theme.font.family.default};
    }
 
-    h1 {
+    h1, h2, h3, h4, h5, h6 {
      font-family: ${({ theme }) => theme.font.family.secondary};
    }
 `
