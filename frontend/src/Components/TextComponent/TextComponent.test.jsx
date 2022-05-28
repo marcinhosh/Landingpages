a479c49066d86ec6574f808077b0da72e1a8b9bc
Marcio <marcinhostudiopro@gmail.com>
2022-05-28 20:40:04 -0300

    test Text component

--- frontend/src/Components/TextComponent/TextComponent.test.jsx
+++ frontend/src/Components/TextComponent/TextComponent.test.jsx
@@ -1,10 +1,14 @@
 import { screen } from '@testing-library/react';
-import { renderTheme } from '../../styles/render-theme';
+import { renderTheme } from '../../styles/renderTheme';
 import { TextComponent } from '.';
 
 describe('<TextComponent />', () => {
     it('should render', () => {
         renderTheme(<TextComponent>Children</TextComponent>);
-        expect(screen.getByRole('heading')).toBeInTheDocument();
+        expect(screen.getByText('Children')).toBeInTheDocument();
+    });
+    it('should render', () => {
+        const {container} = renderTheme(<TextComponent>Children</TextComponent>);
+        expect(container.firstChild).toMatchSnapshot();
     });
 });
